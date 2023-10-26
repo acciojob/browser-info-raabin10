7080961d9aa20b89b4f5efc7002597223889bd9e
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-10-26 23:10:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ const browserName = navigator.appName;
+        const version = navigator.appVersion;
+
+        // Create a message to display the browser information
+        const message = "You are using " + browserName + " version " + version;
+
+        // Display the message in the "browser-info" div
+        const browserInfoDiv = document.getElementById("browser-info");
+        browserInfoDiv.textContent = message;
